360f80838df9c4196fabfd3c81587f931b5f7f35
mehowch <michal.bmx@op.pl>
2019-11-27 19:52:19 +0100

    Bugfix: imports fixed 2.0

--- src/main/java/pl/chmiel/biblioteka/controller/BookController.java
+++ src/main/java/pl/chmiel/biblioteka/controller/BookController.java
@@ -3,10 +3,7 @@ package pl.chmiel.biblioteka.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import pl.chmiel.biblioteka.component.Book;
 import pl.chmiel.biblioteka.repository.BookRepo;
 
